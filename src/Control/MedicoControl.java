bcc1354abc5dd161c053a07df9d6683dca0f0434
ter-9001 <gabrielmartinsgmgm23@gmail.com>
2021-12-03 21:09:01 -0300

    Salvando projeto

---
+++ src/Control/MedicoControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class MedicoControl {
+    
+    
+    public boolean Cadastrar( String nome, String CRM, String Especialidade,  int Telefone, String perido_de_atendimento )
+    {
+       return true; 
+    }
+
+    public Object getMinhaLista() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
